5f99a2800f1d67ee29c17961460e2977c753d417
lyz <706483447@qq.com>
2022-12-09 16:06:23 +0800

    master

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+### 基础算法
+1. quick_sort 快速排序
+2. merge_sort 归并排序
---
+++ quick_sort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+using namespace std;
+
+const int N = 1e5+10;
+
+void quick_sort(int q[], int l, int r)
+{
+    if (l >= r) return;
+
+    int i = l - 1, j = r + 1, x = q[l + r >> 1];
+    while (i < j)
+    {
+        do i ++ ; while (q[i] < x);
+        do j -- ; while (q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+    quick_sort(q, l, j), quick_sort(q, j + 1, r);
+}
+
+int main()
+{
+    int n,q[N];
+    
+    printf("input len: ");
+    scanf("%d",&n);
+
+    printf("input num: ");
+    for(int i = 0 ; i < n ; i++) scanf("%d",&q[i]);
+
+    quick_sort(q, 0, n-1);
+
+    printf("output: ");
+    for(int i = 0; i < n; i++) printf(" %d", q[i]);
+
+    system("pause");
+    return 0;
+
+}
